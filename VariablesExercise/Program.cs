c3f20faa611f2ebce419a66f190dc6221d86bf01
Andrew Olson <andruolson@gmail.com>
2026-02-02 14:49:48 -0700

    Did the homework assignment, declared and initialized all the the types, and put them in a interpolated line of code to show them.

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userName = "Andrew";
+            int userAge = 38;
+            char middleInitial = 'D';
+            bool isAlive = true;
+            double userWallet = 30;
+            decimal userBalance = 20.78m;
+            
+            Console.WriteLine ($"User's name is {userName} and he is {userAge} years old. He has {userWallet} in his wallet." +
+                               $" You may have a question, is the user alive? Well, the answer is {isAlive}... His middle initial" +
+                               $" is {middleInitial} and his current balance is {userBalance}.");
+            
+            
         }
     }
 }
